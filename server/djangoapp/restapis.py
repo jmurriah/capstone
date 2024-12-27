a7dace30e594773caf2b03074cc3889006b1db42
jmurriah <jmurriah@gmail.com>
2024-12-27 07:47:51 -0500

    Pylint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -17,9 +17,9 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -48,8 +48,8 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
         print("Network exception occurred")
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost', 
-    'https://jmurriah-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-    'https://jmurriah-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://jmurriah-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.'
+        'cognitiveclass.ai', 
+    'https://jmurriah-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.'
+        'cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://jmurriah-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-    'https://jmurriah-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://jmurriah-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.'
+        'cognitiveclass.ai', 
+    'https://jmurriah-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.'
+        'cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -101,7 +105,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -150,4 +155,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
